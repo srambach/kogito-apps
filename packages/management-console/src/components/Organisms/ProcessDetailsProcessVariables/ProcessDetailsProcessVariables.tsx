f2ce1ad7b914c8e61f82dbdeb46b1ef614cd8494
Sarah Rambacher <srambach@redhat.com>
2019-11-08 16:54:04 -0500

    Initial structure for process variable layout

--- packages/management-console/src/components/Organisms/ProcessDetailsProcessVariables/ProcessDetailsProcessVariables.tsx
+++ packages/management-console/src/components/Organisms/ProcessDetailsProcessVariables/ProcessDetailsProcessVariables.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, CardFooter, CardHeader, Text, TextContent, TextVariants, Title } from '@patternfly/react-core';
+import { Card, CardBody, CardFooter, CardHeader, Text, TextContent, TextVariants, Title, Flex, FlexItem } from '@patternfly/react-core';
 import React from 'react';
 import ReactJson from 'react-json-view';
+import './ProcessDetailsProcessVariables.css';
 
 const ProcessDetailsProcessVariables = ({ loading, data }) => {
   return (
@@ -9,7 +10,7 @@ const ProcessDetailsProcessVariables = ({ loading, data }) => {
         <Title headingLevel="h3" size="xl">Process Variables</Title>
       </CardHeader>
       <CardBody>
-        <TextContent>
+        {/* <TextContent>
           {!loading ? (
             data.ProcessInstances.map((item, index) => {
               return (
@@ -21,7 +22,94 @@ const ProcessDetailsProcessVariables = ({ loading, data }) => {
           ) : (
             <Text component={TextVariants.h4}>Loading...</Text>
           )}
-        </TextContent>
+        </TextContent> */}
+
+        <Flex>
+          
+          <Flex>
+            <FlexItem className="kie-process-variables__label pf-m-full-width">flight</FlexItem>
+            <Flex className="kie-process-variables__value">
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">arrival</FlexItem>
+                <FlexItem className="kie-process-variables__value">2019-10-22T22:00:00Z[UTC]</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">departure</FlexItem>
+                <FlexItem className="kie-process-variables__value">2019-10-30T22:22:00Z[UTC]</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">flightNumber</FlexItem>
+                <FlexItem className="kie-process-variables__value">MX555</FlexItem>
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <Flex>
+            <FlexItem className="kie-process-variables__label pf-m-full-width">trip</FlexItem>
+            <Flex className="kie-process-variables__value">
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">begin</FlexItem>
+                <FlexItem className="kie-process-variables__value">2019-10-22T22:00:00Z[UTC]</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">city</FlexItem>
+                <FlexItem className="kie-process-variables__value">Bangalore</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">country</FlexItem>
+                <FlexItem className="kie-process-variables__value">India</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">end</FlexItem>
+                <FlexItem className="kie-process-variables__value">2019-10-30T22:22:00Z[UTC]</FlexItem>
+              </Flex>
+              <Flex className="kie-process-variables__tuple">
+                <FlexItem className="kie-process-variables__label">visaRequired</FlexItem>
+                <FlexItem className="kie-process-variables__value">false</FlexItem>
+              </Flex>
+            </Flex>
+          </Flex>
+
+          <Flex>
+            <FlexItem className="kie-process-variables__label pf-m-full-width">hotel</FlexItem>
+            <Flex className="kie-process-variables__value">
+              <Flex>
+                <FlexItem className="kie-process-variables__label pf-m-full-width">address</FlexItem>
+                <Flex className="kie-process-variables__value">
+                  <Flex className="kie-process-variables__tuple">
+                    <FlexItem className="kie-process-variables__label">city</FlexItem>
+                    <FlexItem className="kie-process-variables__value">Bangalore</FlexItem>
+                  </Flex>
+                  <Flex className="kie-process-variables__tuple">
+                    <FlexItem className="kie-process-variables__label">country</FlexItem>
+                    <FlexItem className="kie-process-variables__value">India</FlexItem>
+                  </Flex>
+                  <Flex className="kie-process-variables__tuple">
+                    <FlexItem className="kie-process-variables__label">street</FlexItem>
+                    <FlexItem className="kie-process-variables__value">1232 street</FlexItem>
+                  </Flex>
+                  <Flex className="kie-process-variables__tuple">
+                    <FlexItem className="kie-process-variables__label">zipCode</FlexItem>
+                    <FlexItem className="kie-process-variables__value">12345</FlexItem>
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Flex>
+
+            <Flex className="kie-process-variables__tuple">
+              <FlexItem className="kie-process-variables__label">bookingNumber</FlexItem>
+              <FlexItem className="kie-process-variables__value">XX-01232456</FlexItem>
+            </Flex>
+            <Flex className="kie-process-variables__tuple">
+              <FlexItem className="kie-process-variables__label">name</FlexItem>
+              <FlexItem className="kie-process-variables__value">Perfect Hotel</FlexItem>
+            </Flex>
+            <Flex className="kie-process-variables__tuple">
+              <FlexItem className="kie-process-variables__label">phone</FlexItem>
+              <FlexItem className="kie-process-variables__value">09877654</FlexItem>
+            </Flex>
+          </Flex>
+        </Flex>
       </CardBody>
     </Card>
   );
